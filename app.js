50f994c4f62751ad39e1bc632526411f5e9c344e
LightPlu <lange50300@naver.com>
2024-11-21 00:05:47 +0900

    파일 삭제 후 추가(DB 아이디 비밀번호 삭제)

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+const dbConnect = require("./config/dbConnect");
+
+const app = express();
+
+dbConnect();
+
+// 미들웨어 설정
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors()); // CORS 설정
+app.use(fileUpload()); // 파일 업로드 미들웨어 추가
+app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // 업로드 폴더 정적 제공
+app.use(express.static(path.join(__dirname))); // 정적 파일 폴더 설정
+
+// 라우트 연결
+const productRoutes = require("./routes/product");
+app.use("/api/products", productRoutes);
+const signupRoutes = require("./routes/signup");
+app.use("/api/members", signupRoutes);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// 서버 실행
+app.listen(3000, () => {
+  console.log("3000번 포트에서 서버 실행 중");
+});
